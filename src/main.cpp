bdf1bfc86759a5c5b740fab8537d14a618837b7e
artur <akorneluk@student.agh.edu.pl>
2026-05-03 11:16:17 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,38 @@
+#include "admin.h"
+#include "menu.h"
+#include "data.h"
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Główna funkcja programu - WYPOZYCZALNIA DVD "RETRO"
+int main() {
+  cout<<endl;
+
+  // Wczytuje liste DVD z pliku movies.txt
+  vector<DVD> movies = loadMovies();
+
+  // Wczytuje liste klientow z pliku customers.txt
+  vector<Customer> customers = loadCustomers();
+
+  // Wczytuje / dodaje wypozyczenia dla wczytanych klientow
+  loadRentals(customers);
+  
+  // Administrator
+  Admin admin("artur", "1234");
+
+  // Główna petla programu
+  showMainMenu(movies, customers, admin);
+
+  // Zapiuje aktualny stan DVD do pliku movies.txt
+  saveMovies(movies);
+
+  // Zapisuje aktualna liste klientow do pliku customers.txt
+  saveCustomers(customers);
+
+  // Zapisuje wypozyczenia do pliku rentals.txt 
+  saveRentals(customers);
+  
+  return 0;
+}
